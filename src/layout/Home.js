10f2e125a1a0990c2099976b9658e63b6fce1c16
Noah Peden <noahpeden@gmail.com>
2017-08-16 19:04:45 -0600

    everything done except donate link and styles

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,90 +1,86 @@
-import React, { Component } from 'react'
-import { Map, DirectionsExample } from '../custom'
-import { withGoogleMap, GoogleMap, DirectionsRenderer } from 'google-maps-react'
-
-const styles = {
-  height: '25vh',
-}
-
-const google = window.google
+import React, { Component } from 'react';
+import Parade from '../assets/parade.png';
 
 export default class Home extends Component {
-  constructor() {
-    super()
-  }
-  state = {
-    origin: new google.maps.LatLng(40.5907246, -105.0832341),
-    waypoints: new google.maps.LatLng(40.587395, -105.0850215),
-    directions: null,
-    markers: [
-      {
-        position: {
-          lat: 40.58526,
-          lng: -105.084423,
-        },
-        key: `Taiwan`,
-        defaultAnimation: 2,
-      },
-    ],
-  }
-  componentDidMount() {
-    const DirectionsService = new google.maps.DirectionsService()
+	constructor() {
+		super();
 
-    DirectionsService.route(
-      {
-        origin: this.state.origin,
-        waypoints: this.state.waypoints,
-        travelMode: google.maps.TravelMode.DRIVING,
-      },
-      (result, status) => {
-        if (status === google.maps.DirectionsStatus.OK) {
-          this.setState({
-            directions: result,
-          })
-        } else {
-          console.error(`error fetching directions ${result}`)
-        }
-      }
-    )
-  }
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <div>
-        Parade Route
-        <Map
-          zoom={12}
-          center={{
-            lat: 40.58526,
-            lng: -105.084423,
-          }}
-          markers={this.state.markers}
-          containerElement={<div style={styles} />}
-          mapElement={<div style={styles} />}
-        />
-        Bars
-        <Map
-          zoom={12}
-          center={{
-            lat: 40.58526,
-            lng: -105.084423,
-          }}
-          containerElement={<div style={styles} />}
-          mapElement={<div style={styles} />}
-        />
-        Directions
-        <Map
-          zoom={12}
-          center={{
-            lat: 40.58526,
-            lng: -105.084423,
-          }}
-          containerElement={<div style={styles} />}
-          mapElement={<div style={styles} />}
-          center={this.state.origin}
-          directions={this.state.directions}
-        />
-      </div>
-    )
-  }
+	render() {
+		const styles = {
+			width: '50%',
+			height: '200px'
+		};
+		return (
+			<div>
+				<h1>Tour de Fat Info!</h1>
+				<iframe
+					src="https://www.google.com/maps/d/u/0/embed?mid=18qU3uZNrfZDMEBuSRMMzNt8H9LI"
+					width="640"
+					height="480"
+				/>
+        <br/>
+				<iframe
+					src="https://www.google.com/maps/d/u/0/embed?mid=1ZxedkP0lEY70O0U-25--y88IzSc"
+					width="440"
+					height="480"
+				/>
+				<h1>Basic info about nonprofits</h1>
+				<ul>
+					<h2>Bike Fort Collins advocates for and leads:</h2>
+					<li>
+						Increased participation in active transportation: NoCo Bike Show,
+						Bicycle Friendly Business program, Bike Share
+					</li>
+					<li>
+						Safer streets and communities: SRTS, support more bicycle friendly
+						infrastructure
+					</li>
+					<li>
+						Inclusive culture and platform for all riders: Pathways to Health,
+						Chain Reaction, KP neighborhood project
+					</li>
+				</ul>
+				<ul>
+					<h2>Overland Mountain Bike Club</h2>
+					<li>
+						"Tour de Fat support enables Overland Mountain Bike Club to pursue
+						it's mission to promote the active, healthy sport of mountain biking
+						by providing education and assistance to all trail users, and by
+						working to build and maintain quality, sustainable trails that
+						enhance the mountain biking experience. We ride, we connect and we
+						contribute!"
+					</li>
+				</ul>
+				<ul>
+					<h2>Fort Collins Bycycle Co-op</h2>
+					<li>
+						The Fort Collins Bike Co-op strives to keep our community biking
+						through educating people on fixing and maintaining their bikes,
+						providing bikes to those not able to afford a bike, and keeping
+						bikes out of the landfill. We also refurbish and donate bikes to
+						charity events and programs. Tour de Fat has helped us put a roof
+						over our heads in order to serve the community
+					</li>
+				</ul>
+				<ul>
+					<h2>Ciclismo Youth Foundation</h2>
+					<li>
+						The Tour de Fat enables the Ciclismo Youth Foundation to fulfill our
+						mission of making cycling opportunities accessible to youth in
+						Northern Colorado. Our youth cycling events and programs support the
+						social and emotional development of children. We aim to teach
+						children life skills by introducing children to cycling in a
+						positive atmosphere that allows them the opportunity to grow and
+						have fun.
+					</li>
+				</ul>
+
+
+        <a>Link to Donate</a>
+			</div>
+		);
+	}
 }
